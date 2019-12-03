8aa28877656b9b930c26328c20fd1bb0a371c6fe
mltlima <54447895+mltlima@users.noreply.github.com>
2019-12-03 11:34:59 -0300

    Update Demo.c

--- Demo.c
+++ Demo.c
@@ -399,7 +399,7 @@ void jogoDemo(void)
 					}
                     }
 
-                printf(CIA"\n Porfavor, aperte alguma tecla para continuar"RESET);
+                printf(CIA"\n Pressione Enter para continuar"RESET);
 				getchar();
                 printf(CLEAN);   // limpa terminal
 				if((pontoComputador == 75 )|| (pontoJogador == 75)){
